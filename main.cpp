46734a53df56a359a6399f7befd361d8081130e7
Ryanakbar10 <113197364+Ryanakbar10@users.noreply.github.com>
2022-09-14 21:06:45 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "BIODATA DIRI!" << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << "NAMA   : RYAN AKBAR RAFSANJANI" << endl;
+    cout << "NIM    : A11.2022.14697" << endl;
+    cout << "ALAMAT : JL. TAMAN TLOGOMULYO GG.2" << endl;
+    cout << "TTL    : SEMARANG 9 JULY 2004" << endl;
+    cout << "HOBI   : BERMAIN GAME" << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    return 0;
+}
